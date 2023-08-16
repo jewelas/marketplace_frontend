759912b84334b0603a4ead483bf4138ba3526cb9
jewelas <jewelas.tm@gmail.com>
2023-08-16 01:52:56 -0400

    fix tab

--- src/js/modules/config.js
+++ src/js/modules/config.js
@@ -3,8 +3,8 @@ import axios from "axios"
 class Config {
     constructor() {
         axios.defaults.baseURL = "https://testserver.artwise.dog/"
-        // axios.defaults.baseURL = "http://202.61.249.136:5000"
-        // axios.defaults.baseURL = "http://localhost:5000"
+        //axios.defaults.baseURL = "http://202.61.249.136:5000"
+        //axios.defaults.baseURL = "http://localhost:5000"
     }
 }
 
